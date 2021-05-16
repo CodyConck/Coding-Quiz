f755c0e19eb76ab85353ff9e7ab86a11ed51b069
CodyConck <codyconck@gmail.com>
2021-05-16 11:37:07 -0500

    added most of ths game functions and updated html

---
+++ assets/js/question.js
@@ -0,0 +1,65 @@
+// The array of questions for the game.
+var quiz = [
+  {
+    question: 'You can add JS directly to an HTML file.',
+    answers: ['True', 'False'],
+    correctAnswer: 'True'
+  },
+  {
+    question: 'What does CSS stand for?',
+    answers: ['Correct Style Sequence', 'Common Sense Styles', 'Cascading Style Sheet', 'Core Sample Styles'],
+    correctAnswer: 'Cascading Style Sheet'
+  },
+  {
+    question: 'Bootstrap is primarily used for ______',
+    answers: ['Finding answers to coding questions', 'Shortcut for webpage styling', 'An alternative to j query', 'Picking yourself up'],
+    correctAnswer: 'Shortcut for webpage styling'
+  },
+  {
+    question: 'Moment.js is a tool used to do what?',
+    answers: ['Take a picture of a moment to remember', 'Give yourself time until the next challenge',
+      'Tell the webpage to take a "moment" to load', 'Calculate and display times in JavaScript'],
+    correctAnswer: 'Calculate and display times in JavaScript'
+  },
+  {
+    question: 'Coding bootcamp is a breeze',
+    answers: ['True', 'False'],
+    correctAnswer: 'False'
+  }
+];
+
+// We start the game with a score of 0.
+//var score = 0;
+
+//function to display the questions
+//function displayQuiz(questionObject) {
+
+  //displayQues.text(questionObject.q);
+  //quesContainer.append(displayQues);
+//}
+
+//displayQuiz()
+
+//start.addEventListener('click', displayQuestion);
+
+// Loop over every question object
+//for (var i = 0; i < questions.length; i++) {
+  // Display current question to user and ask OK/Cancel
+  //var answer = (questions[i].q);
+//}
+  // Compare answers
+  //if (
+   // (answer === true && questions[i].a === 't') ||
+    //(answer === false && questions[i].a === 'f')
+  //) {
+    // Increase score
+    //score++;
+    // Alert the user
+   // alert('Correct!');
+ // } else {
+    //alert('Wrong!');
+  
+//}
+
+  // Show total at end
+  //alert('You got ' + score + '/' + questions.length);
